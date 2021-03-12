20b293cc6410ee0bf8804aea056c4702fbd2a885
Cedric Gava <cedric@cgava-consulting.com>
2021-03-12 14:25:14 +0100

    preparing for merge with dedicated_generator

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-from pyecore.resources import ResourceSet, URI, global_registry
-
-import pyecore.ecore as Ecore
-import pyEcoreCapella as pyEcoreCapella
-
-rset = ResourceSet()
-resource = rset.get_resource(URI('./test.empty.project/test.empty.project.melodymodeller'))
